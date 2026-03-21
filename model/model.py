2e0e3de14477915af9099a5489c422c0ee70a5b7
Soyorin-suki <2374237963@qq.com>
2026-03-21 16:14:42 +0800

    feat: FeedForward

--- model/model.py
+++ model/model.py
@@ -349,6 +349,27 @@ class Attention(nn.Module):
 		return output, past_kv
 
 
+class FeedForward(nn.Module):
+
+	def __init__(
+		self,
+		args: MyModelConfig
+	):
+		super().__init__()
+		if args.intermediate_size is None:
+			intermediate_size = int(args.hidden_size * 8 / 3)
+			args.intermediate_size = 64 * ((intermediate_size+64-1)//64)
+
+		self.up_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias=False)
+		self.down_proj = nn.Linear(args.intermediate_size, args.hidden_size, bias=False)
+		self.gate_proj = nn.Linear(args.hidden_size, args.intermediate_size, bias=False)
+		self.dropout = nn.Dropout(args.dropout)
+		self.act_fn = ACT2FN[args.hidden_act]
+
+	def forward(self, x: torch.Tensor):
+		return self.dropout(self.down_proj(self.act_fn(self.gate_proj(x))*self.up_proj(x)))
+
+
 
 
 
